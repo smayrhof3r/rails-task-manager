26adbf18b506c4648f51efee1db2dff4915d4a82
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-07-16 14:32:24 +0100

    completed basic CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,11 +11,28 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def update
+  def create
+    redirect_to task_path(Task.create(task_params))
+  end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path(task)
   end
 
   def destroy
+    Task.find(params[:id]).destroy
+    redirect_to tasks_path, status: :see_other
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'index', to: 'tasks#index', as: 'tasks'
-  get 'show/:id', to: 'tasks#show', as: 'task'
-  get 'new', to: 'tasks#new'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
+  resources :tasks
 end
